166c21b55fabb77c3c249d2e42fbfe7f62b07dad
Adam Romero <adamromero89@gmail.com>
2023-07-13 17:01:35 -0700

    Add Patreon button

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,18 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
+import PatreonProvider from "next-auth/providers/patreon";
 
 const handler = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-  ],
+   providers: [
+      GithubProvider({
+         clientId: process.env.GITHUB_CLIENT_ID,
+         clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      }),
+      PatreonProvider({
+         clientId: process.env.PATREON_CLIENT_ID,
+         clientSecret: process.env.PATREON_CLIENT_SECRET,
+      }),
+   ],
 });
 
 export { handler as GET, handler as POST };
