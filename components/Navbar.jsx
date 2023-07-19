44f078b89423b3a8b7b6b1ec88041ddb3f13a6e8
Adam Romero <adamromero89@gmail.com>
2023-07-19 13:54:02 -0700

    Change color of navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import ProfileIcon from "./ProfileIcon";
 
 export default function Navbar() {
-  return (
-    <nav className="flex items-center justify-between fixed left-0 right-0 px-8 h-14 bg-blue-500 text-white">
-      <Link className="font-bold" href={"/"}>
-        GTCoding.
-      </Link>
-      <ProfileIcon />
-    </nav>
-  );
+   return (
+      <nav className="flex items-center justify-between fixed left-0 right-0 px-8 h-14 bg-green-500 text-white">
+         <Link className="font-bold" href={"/"}>
+            Home
+         </Link>
+         <ProfileIcon />
+      </nav>
+   );
 }
