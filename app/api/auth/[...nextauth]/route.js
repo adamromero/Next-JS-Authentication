4e03cf732e97b670d8684f0ccfaafdb8aece23af
Adam Romero <adamromero89@gmail.com>
2023-07-20 10:47:22 -0700

    Remove callbacks

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,30 +9,17 @@ const handler = NextAuth({
          clientSecret: process.env.GITHUB_CLIENT_SECRET,
       }),
       PatreonProvider({
+         clientId: process.env.PATREON_CLIENT_ID,
+         clientSecret: process.env.PATREON_CLIENT_SECRET,
          authorization: {
-            //url: `${process.env.PATREON_AUTHORIZE_URL}?response_type=code`,
             params: {
                redirect_uri: "https://www.nightmarecarvings.com",
                scope: "identity identity.memberships",
                grant_type: "authorization_code",
             },
          },
-         clientId: process.env.PATREON_CLIENT_ID,
-         clientSecret: process.env.PATREON_CLIENT_SECRET,
       }),
    ],
-   callbacks: {
-      async jwt(token, user) {
-         if (user) {
-            token.accessToken = user.accessToken;
-         }
-         return token;
-      },
-      async session(session, token) {
-         session.accessToken = token.accessToken;
-         return session;
-      },
-   },
 });
 
 export { handler as GET, handler as POST };
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
+"use client";
 import Header from "@/components/Header";
 
 export default function Home() {
-  return <Header />;
+   return <Header />;
 }
