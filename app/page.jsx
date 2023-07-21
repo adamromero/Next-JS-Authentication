1e26db173478cdb6d1ea1367d695addb8ed8873a
Adam Romero <adamromero89@gmail.com>
2023-07-21 15:22:52 -0700

    Remove experiment

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,7 @@
 import Header from "@/components/Header";
 
-const getPostsData = async ({ code }) => {
-   const data = {
-      code,
-      grant_type: "authorization_code",
-      client_id: process.env.PATREON_CLIENT_ID,
-      client_secret: process.env.PATREON_CLIENT_SECRET,
-      redirect_uri: "https://www.nightmarecarvings.com",
-   };
-
-   const res = await fetch("https://www.patreon.com/api/oauth2/token", {
-      method: "POST",
-      headers: {
-         "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: data,
-   });
-   return res.json();
-};
-
 export default async function Home({ searchParams }) {
-   const token = await getPostsData(searchParams.code);
-   console.log(token);
+   console.log(searchParams.code);
 
-   return (
-      <>
-         <Header />
-      </>
-   );
+   return <Header />;
 }
