24b03ed186c0fd04a5d344e82058294370a99778
Adam Romero <adamromero89@gmail.com>
2023-07-24 10:28:19 -0700

    Trying something

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,23 +4,50 @@ import PatreonProvider from "next-auth/providers/patreon";
 
 const handler = NextAuth({
    providers: [
-      GithubProvider({
-         clientId: process.env.GITHUB_CLIENT_ID,
-         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      }),
-      PatreonProvider({
-         clientId: process.env.PATREON_CLIENT_ID,
-         clientSecret: process.env.PATREON_CLIENT_SECRET,
+      // GithubProvider({
+      //    clientId: process.env.GITHUB_CLIENT_ID,
+      //    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      // }),
+      // PatreonProvider({
+      //    clientId: process.env.PATREON_CLIENT_ID,
+      //    clientSecret: process.env.PATREON_CLIENT_SECRET,
+      //    authorization: {
+      //       params: {
+      //          redirect_uri: "https://www.nightmarecarvings.com",
+      //          scope: "identity identity.memberships",
+      //          grant_type: "authorization_code",
+      //       },
+      //    },
+      // }),
+      {
+         id: "patreon",
+         name: "PATREON",
+         type: "oauth",
+         version: "2.0",
          authorization: {
-            url: process.env.PATREON_AUTHORIZE_URL,
+            url: `${process.env.PATREON_AUTHORIZE_URL}?response_type=code`,
             params: {
-               redirect_uri: "https://www.nightmarecarvings.com",
-               scope: "identity identity.memberships",
+               scope: "identity identity[email] identity.memberships",
                grant_type: "authorization_code",
             },
          },
-         token: process.env.PATREON_TOKEN_URL,
-      }),
+         token: {
+            url: `${process.env.PATREON_TOKEN_URL}`,
+         },
+         userinfo: {
+            url: `${process.env.PATREON_PROFILE_URL}`,
+         },
+         profile: (profile) => {
+            return {
+               id: profile.data.id,
+               name: profile.data.attributes.full_name,
+               email: profile.data.attributes.email,
+               provider: "PATREON",
+            };
+         },
+         clientId: process.env.PATREON_CLIENT_ID,
+         clientSecret: process.env.PATREON_CLIENT_SECRET,
+      },
    ],
 });
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,47 +1,47 @@
 import Header from "@/components/Header";
 
 export default async function Home({ searchParams }) {
-   if (searchParams.code) {
-      console.log(searchParams.code);
-      const authorizationCode = searchParams.code;
-      const clientId = process.env.PATREON_CLIENT_ID;
-      const clientSecret = process.env.PATREON_CLIENT_SECRET;
-      const redirectUri = process.env.PATREON_REDIRECT_URI;
-      const tokenUrl = "https://www.patreon.com/api/oauth2/token";
+   // if (searchParams.code) {
+   //    console.log(searchParams.code);
+   //    const authorizationCode = searchParams.code;
+   //    const clientId = process.env.PATREON_CLIENT_ID;
+   //    const clientSecret = process.env.PATREON_CLIENT_SECRET;
+   //    const redirectUri = process.env.PATREON_REDIRECT_URI;
+   //    const tokenUrl = "https://www.patreon.com/api/oauth2/token";
 
-      const data = new URLSearchParams();
-      data.append("code", authorizationCode);
-      data.append("grant_type", "authorization_code");
-      data.append("client_id", clientId);
-      data.append("client_secret", clientSecret);
-      data.append("redirect_uri", redirectUri);
+   //    const data = new URLSearchParams();
+   //    data.append("code", authorizationCode);
+   //    data.append("grant_type", "authorization_code");
+   //    data.append("client_id", clientId);
+   //    data.append("client_secret", clientSecret);
+   //    data.append("redirect_uri", redirectUri);
 
-      fetch(tokenUrl, {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-         },
-         body: data,
-      })
-         .then((response) => response.json())
-         .then((tokenData) => {
-            console.log("token:", tokenData);
-            const accessToken = tokenData.access_token;
+   //    fetch(tokenUrl, {
+   //       method: "POST",
+   //       headers: {
+   //          "Content-Type": "application/x-www-form-urlencoded",
+   //       },
+   //       body: data,
+   //    })
+   //       .then((response) => response.json())
+   //       .then((tokenData) => {
+   //          console.log("token:", tokenData);
+   //          const accessToken = tokenData.access_token;
 
-            fetch("https://www.patreon.com/api/oauth2/api/current_user", {
-               headers: {
-                  Authorization: `Bearer ${accessToken}`,
-               },
-            })
-               .then((res) => res.json())
-               .then((userData) => {
-                  console.log(userData);
-               });
-         })
-         .catch((error) => {
-            console.log("Token exchange failed.. Error:", error);
-         });
-   }
+   //          fetch("https://www.patreon.com/api/oauth2/api/current_user", {
+   //             headers: {
+   //                Authorization: `Bearer ${accessToken}`,
+   //             },
+   //          })
+   //             .then((res) => res.json())
+   //             .then((userData) => {
+   //                console.log(userData);
+   //             });
+   //       })
+   //       .catch((error) => {
+   //          console.log("Token exchange failed.. Error:", error);
+   //       });
+   // }
 
    return (
       <>
