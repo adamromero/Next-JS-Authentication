bb5e7d21b55fa2dc451be678881ab55bb48847fd
Adam Romero <adamromero89@gmail.com>
2023-07-24 15:42:13 -0700

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
+   images: {
+      domains: [
+         "avatars.githubusercontent.com",
+         "avatars.patreonusercontent.com",
+      ],
+   },
 };
 
 module.exports = nextConfig;
