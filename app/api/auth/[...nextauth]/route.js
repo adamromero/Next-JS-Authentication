4cabdd15698c66d111277a49478a38c52ec3d989
Adam Romero <adamromero89@gmail.com>
2023-07-24 11:15:56 -0700

    add response type

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,51 +4,22 @@ import PatreonProvider from "next-auth/providers/patreon";
 
 const handler = NextAuth({
    providers: [
-      // GithubProvider({
-      //    clientId: process.env.GITHUB_CLIENT_ID,
-      //    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      // }),
-      // PatreonProvider({
-      //    clientId: process.env.PATREON_CLIENT_ID,
-      //    clientSecret: process.env.PATREON_CLIENT_SECRET,
-      //    authorization: {
-      //       params: {
-      //          redirect_uri: "https://www.nightmarecarvings.com",
-      //          scope: "identity identity.memberships",
-      //          grant_type: "authorization_code",
-      //       },
-      //    },
-      // }),
-      {
-         id: "patreon",
-         name: "PATREON",
-         type: "oauth",
-         version: "2.0",
-         authorization: {
-            url: `${process.env.PATREON_AUTHORIZE_URL}?response_type=code`,
-            params: {
-               redirect_uri: "https://www.nightmarecarvings.com",
-               scope: "identity identity[email] identity.memberships",
-               grant_type: "authorization_code",
-            },
-         },
-         token: {
-            url: `${process.env.PATREON_TOKEN_URL}`,
-         },
-         userinfo: {
-            url: `${process.env.PATREON_PROFILE_URL}`,
-         },
-         profile: (profile) => {
-            return {
-               id: profile.data.id,
-               name: profile.data.attributes.full_name,
-               email: profile.data.attributes.email,
-               provider: "PATREON",
-            };
-         },
+      GithubProvider({
+         clientId: process.env.GITHUB_CLIENT_ID,
+         clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      }),
+      PatreonProvider({
          clientId: process.env.PATREON_CLIENT_ID,
          clientSecret: process.env.PATREON_CLIENT_SECRET,
-      },
+         authorization: {
+            params: {
+               redirect_uri: "https://www.nightmarecarvings.com",
+               scope: "identity identity.memberships",
+               grant_type: "authorization_code",
+               response_type: "code",
+            },
+         },
+      }),
    ],
 });
 
