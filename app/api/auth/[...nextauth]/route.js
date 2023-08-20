ca137f5d4543a2b38e351d0a10a74e5a6acaab68
Adam Romero <adamromero89@gmail.com>
2023-08-20 13:25:13 -0700

    Add mongodb adapter

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import PatreonProvider from "next-auth/providers/patreon";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 const handler = NextAuth({
    providers: [
@@ -13,6 +15,13 @@ const handler = NextAuth({
          clientSecret: process.env.PATREON_CLIENT_SECRET,
       }),
    ],
+   adapter: MongoDBAdapter(clientPromise),
+   session: {
+      strategy: "jwt",
+   },
+   jwt: {
+      secret: process.env.JWT_SECRET,
+   },
 });
 
 export { handler as GET, handler as POST };
