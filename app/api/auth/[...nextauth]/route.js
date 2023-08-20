7e472cee9697754661f595445df0b18ca159ed11
Adam Romero <adamromero89@gmail.com>
2023-08-20 12:51:42 -0700

    Revert to previous commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,40 +1,13 @@
 import NextAuth from "next-auth/next";
-import PatreonProvider from "next-auth/providers/patreon";
 import GithubProvider from "next-auth/providers/github";
-import axios from "axios";
 
-export const nextAuthOptions = {
+const handler = NextAuth({
    providers: [
       GithubProvider({
          clientId: process.env.GITHUB_CLIENT_ID,
          clientSecret: process.env.GITHUB_CLIENT_SECRET,
       }),
-      PatreonProvider({
-         clientId: process.env.PATREON_CLIENT_ID,
-         clientSecret: process.env.PATREON_CLIENT_SECRET,
-      }),
    ],
-   pages: {
-      signIn: "/",
-   },
-   secret: process.env.NEXTAUTH_SECRET,
-   callbacks: {
-      async jwt({ token, account, profile }) {
-         if (account) {
-            token.accessToken = account.access_token;
-            token.id = profile.id;
-         }
-         return token;
-      },
-      async session({ token, session }) {
-         return session;
-      },
-      async redirect({ url, baseUrl }) {
-         return baseUrl;
-      },
-   },
-};
-
-const handler = NextAuth(nextAuthOptions);
+});
 
 export { handler as GET, handler as POST };
--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -3,21 +3,12 @@
 import { signIn } from "next-auth/react";
 
 export default function LoginButton() {
-   return (
-      <>
-         <button
-            onClick={() => signIn("github")}
-            className="mt-4 bg-slate-800 text-white px-6 py-3 rounded-lg"
-         >
-            Sign in with Github
-         </button>
-
-         <button
-            onClick={() => signIn("patreon")}
-            className="mt-4 bg-slate-800 text-white px-6 py-3 rounded-lg"
-         >
-            Connect with Patreon
-         </button>
-      </>
-   );
+  return (
+    <button
+      onClick={() => signIn("github")}
+      className="mt-4 bg-slate-800 text-white px-6 py-3 rounded-lg"
+    >
+      Sign in with Github
+    </button>
+  );
 }
