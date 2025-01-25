9e2e9403302e006e80a90bfc0f3bd5a6ad3d8440
Adam Romero <adamromero89@gmail.com>
2025-01-25 14:35:08 -0800

    Set up post for movie posts vs tv posts

--- app/api/patreon-webhook/route.js
+++ app/api/patreon-webhook/route.js
@@ -1,24 +1,78 @@
 import { NextResponse } from "next/server";
 
-const parseMovieTitleAndYear = (input) => {
+const extractTitle = (input) => {
    const match = input.match(/^(.*?) \((\d{4})\)/);
-   if (match) {
-      const parsedTitle = match[1];
-      const year = match[2];
-      return { parsedTitle, year };
-   }
-   return null;
+   return match ? match[1] : "";
+};
+
+const extractYear = (input) => {
+   const match = input.match(/^(.*?) \((\d{4})\)/);
+   return match ? match[2] : "";
 };
 
 export async function POST(req, res) {
    const request = await req.json();
    const { title, url, published_at } = request.data.attributes;
 
-   if (title.includes("Full Length")) {
-      const { parsedTitle, year } = parseMovieTitleAndYear(title);
-      console.log(
-         `title: ${parsedTitle}, year: ${year}, url: ${url}, published_at: ${published_at}`
-      );
+   if (title.includes("Full Length Reaction")) {
+      const extractedTitle = extractTitle(title).trim();
+      const year = extractYear(title).trim();
+      if (extractedTitle) {
+         //query mongodb movies collection using parsedTitle and year
+         //check that the data.Type is "movie"
+         //check that the document properties !hasReacted and !hasSeen
+         //set hasReacted to true
+         //set links.patreon to url
+         //set publishedAt to published_at
+         if (year) {
+            // await Movie.find(
+            //    {
+            //       "data.Title": extractedTitle,
+            //       "data.Year": year,
+            //       "data.Type": "movie",
+            //       hasReacted: false,
+            //       hasSeen: false,
+            //    },
+            //    {
+            //       $set: {
+            //          hasReacted: true,
+            //          "links.patreon": url,
+            //          publishedAt: published_at,
+            //       },
+            //    }
+            // );
+            console.log({
+               title: extractTitle,
+               year,
+               url,
+               published_at,
+               type: "movie",
+            });
+         } else {
+            //query query mongodb movies collection using parsedTitle
+            //check that the data.Type is "tv"
+            //check that the document properties !hasReacted and !hasSeen
+            //set hasReacted to true
+            //set links.patreon to url
+            //set publishedAt to published_at
+            // await Movie.find(
+            //    {
+            //       "data.Title": extractedTitle,
+            //       "data.Type": "tv",
+            //       hasReacted: false,
+            //       hasSeen: false,
+            //    },
+            //    {
+            //       $set: {
+            //          hasReacted: true,
+            //          "links.patreon": url,
+            //          publishedAt: published_at,
+            //       },
+            //    }
+            // );
+            console.log({ title: extractTitle, url, published_at, type: "tv" });
+         }
+      }
    }
 
    return NextResponse.json({
