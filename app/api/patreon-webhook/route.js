9ca5c0c339a5acd970261f0a69207fb37bcf43a0
Adam Romero <adamromero89@gmail.com>
2025-01-24 17:13:16 -0800

    Destructure and log needed data

--- app/api/patreon-webhook/route.js
+++ app/api/patreon-webhook/route.js
@@ -1,8 +1,26 @@
 import { NextResponse } from "next/server";
 
+const parseMovieTitleAndYear = (input) => {
+   const match = input.match(/^(.*?) \((\d{4})\)/);
+   if (match) {
+      const parsedTitle = match[1];
+      const year = match[2];
+      return { parsedTitle, year };
+   }
+   return null;
+};
+
 export async function POST(req, res) {
    const request = await req.json();
-   console.log(request);
+   const { title, url, published_at } = request.data.attributes;
+
+   if (title.includes("Full Length")) {
+      const { parsedTitle, year } = parseMovieTitleAndYear(title);
+      console.log(
+         `title: ${parsedTitle}, year: ${year}, url: ${url}, published_at: ${published_at}`
+      );
+   }
+
    return NextResponse.json({
       message: "hello there",
    });
