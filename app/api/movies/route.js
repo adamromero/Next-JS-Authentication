cf2783fde1afb26d179a343858ebaba374af59a4
Adam Romero <adamromero89@gmail.com>
2025-01-25 23:00:03 -0800

    set headers for response to no-store

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -7,7 +7,9 @@ connectDB();
 export async function GET(req, res) {
    try {
       const movieVotes = await Movie.find();
-      return NextResponse.json(movieVotes);
+      const response = NextResponse.json(movieVotes);
+      response.headers.set("Cache-Control", "no-store");
+      return response;
    } catch (error) {
       return NextResponse.json({
          error: "Unable to fetch movie votes from the database.",
